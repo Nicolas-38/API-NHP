2cc907c7864976e162e7bd187943913f8e9a1c5f
Nicolas Rombaldi <nicolasrombaldi24@gmail.com>
2025-04-09 17:24:48 -0300

    criar o server.js

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import http from'node:http'
+
+//para importação http pode ser usado const http = requeri('http') ou import http from'http'
+//comomJS => require
+//Aplicações HTTP = APIs
+
+const server = http.createServer((req, res) => {
+
+})
+
+server.listen(3333)
